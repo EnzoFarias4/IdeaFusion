4dba2a672c524c208fdc72fb4c13e5bf250347b2
EnzoFarias4 <102108717+EnzoFarias4@users.noreply.github.com>
2024-07-09 19:05:57 +0300

    improve variables

--- routes.py
+++ routes.py
@@ -1,26 +1,26 @@
 from flask import Blueprint, request
-from your_idea_controller_module import create_idea, get_all_ideas, get_idea, update_idea, delete_idea
+from your_idea_controller_module import create_new_idea, fetch_all_ideas, fetch_single_idea, update_existing_idea, remove_existing_idea
 
-ideas_blueprint = Blueprint('ideas', __name__)
+ideas_api_blueprint = Blueprint('ideas_api', __name__)
 
-@ideas_blueprint.route('/ideas', methods=['POST'])
-def add_idea():
-    data = request.get_json()
-    return create_idea(data)
+@ideas_api_blueprint.route('/ideas', methods=['POST'])
+def add_new_idea():
+    idea_data = request.get_json()
+    return create_new_idea(idea_data)
 
-@ideas_blueprint.route('/ideas', methods=['GET'])
-def retrieve_ideas():
-    return get_all_ideas()
+@ideas_api_blueprint.route('/ideas', methods=['GET'])
+def get_ideas():
+    return fetch_all_ideas()
 
-@ideas_Register_blueprint.route('/ideas/<idea_id>', methods=['GET'])
-def retrieve_idea(idea_id):
-    return get_idea(idea_id)
+@ideas_api_blueprint.route('/ideas/<idea_id>', methods=['GET'])
+def get_specific_idea(idea_id):
+    return fetch_single_idea(idea_id)
 
-@ideas_blueprint.route('/ideas/<idea_id>', methods=['PUT'])
-def modify_idea(idea_id):
-    data = request.get_json()
-    return update_idea(idea_id, data)
+@ideas_api_blueprint.route('/ideas/<idea_id>', methods=['PUT'])
+def update_idea(idea_id):
+    updated_idea_data = request.get_json()
+    return update_existing_idea(idea_id, updated_idea_data)
 
-@ideas_blueprint.route('/ideas/<idea_id>', methods=['DELETE'])
-def remove_idea(idea_id):
-    return delete_idea(idea_id)
+@ideas_api_blueprint.route('/ideas/<idea_id>', methods=['DELETE'])
+def delete_idea(idea_id):
+    return remove_existing_idea(idea_id)
