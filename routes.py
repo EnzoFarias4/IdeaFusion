47367d674004f41fbf895554e99c55582197b5e9
EnzoFarias4 <102108717+EnzoFarias4@users.noreply.github.com>
2024-07-12 13:23:00 +0300

    new feature

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from your_idea_controller_module import create_new_idea, fetch_all_ideas, fetch_single_idea, update_existing_idea, remove_existing_idea
+from your_idea_controller_module import create_new_idea, fetch_all_ideas, fetch_single_idea, update_existing_idea, remove_existing_idea, rate_idea, fetch_idea_rating
 
 ideas_api_blueprint = Blueprint('ideas_api', __name__)
 
@@ -24,3 +24,31 @@ def update_idea(idea_id):
 @ideas_api_blueprint.route('/ideas/<idea_id>', methods=['DELETE'])
 def delete_idea(idea_id):
     return remove_existing_idea(idea_id)
+
+@ideas_api_blueprint.route('/ideas/<idea_id>/rate', methods=['POST'])
+def rate_an_idea(idea_id):
+    rating_data = request.get_json()
+    return rate_idea(idea_id, rating_data)
+
+@ideas_api_blueprint.route('/ideas/<idea_id>/rating', methods=['GET'])
+def get_idea_rating(idea_id):
+    return fetch_idea_rating(idea_id)
+```
+
+```python
+def rate_idea(idea_id, rating_data):
+    """
+    Update the provided idea with a new rating.
+    :param idea_id: The ID of the idea to be rated.
+    :param ratingody): JSON containing the rating details.
+    :return: A response indicating the success or failure of the operation.
+    """
+    pass
+
+def fetch_idea_rating(idea_id):
+    """
+    Retrieve the average rating of a specific idea.
+    :param idea_id: The ID of the idea.
+    :return: The average rating of the idea.
+    """
+    pass
