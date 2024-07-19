f5745d1e190018477767106814be1143a91802ab
EnzoFarias4 <102108717+EnzoFarias4@users.noreply.github.com>
2024-07-19 20:27:09 +0300

    change names

--- models.py
+++ models.py
@@ -5,24 +5,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_CONNECTION_URL = os.getenv("DATABASE_CONNECTION_URL")
 
 db = SQLAlchemy()
 
-class Idea(db.Model):
-    __tablename__ = 'ideas'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
-    description = db.Column(db.Text, nullable=False)
-    connected_ideas = relationship(
-        'Idea',
-        secondary='idea_connections',
-        primaryjoin='Idea.id==idea_connections.c.idea_id',
-        secondaryjoin='Idea.id==idea_connections.c.connected_idea_id',
-        backref='connections'
+class CreativeIdea(db.Model):
+    __tablename__ = 'creative_ideas'
+    idea_id = db.Column(db.Integer, primary_key=True)
+    idea_title = db.Column(db.String(255), nullable=False)
+    idea_description = db.Column(db.Text, nullable=False)
+    related_ideas = relationship(
+        'CreativeIdea',
+        secondary='idea_linkage',
+        primaryjoin='CreativeIdea.idea_id==idea_linkage.c.initiator_idea_id',
+        secondaryjoin='CreativeIdea.idea_id==idea_linkage.c.related_idea_id',
+        backref='related_connections'
     )
 
-idea_connections = db.Table('idea_connections',
-    db.Column('idea_id', db.Integer, db.ForeignKey('ideas.id'), primary_key=True),
-    db.Column('connected_idea_id', db.Integer, db.ForeignKey('ideas.id'), primary_key=True)
+idea_linkage = db.Table('idea_linkage',
+    db.Column('initiator_idea_id', db.Integer, db.ForeignKey('creative_ideas.idea_id'), primary_key=True),
+    db.Column('related_idea_id', db.Integer, db.ForeignKey('creative_ideas.idea_id'), primary_key=True)
 )
