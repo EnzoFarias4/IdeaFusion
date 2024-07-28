cc78f8642c997d20731ab95190e924886c832b4c
EnzoFarias4 <102108717+EnzoFarias4@users.noreply.github.com>
2024-07-28 23:38:20 +0300

    change variable names

--- db.py
+++ db.py
@@ -6,27 +6,28 @@ from functools import lru_cache
 
 load_dotenv()
 
-DATABASE_USER = os.getenv('DATABASE_USER')
-DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
-DATABASE_HOST = os.getenv('DATABASE_HOST')
-DATABASE_PORT = os.getenv('DATABASE_PORT', '5432')
-DATABASE_NAME = os.getenv('DATABASE_NAME')
-DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+db_user = os.getenv('DATABASE_USER')
+db_password = os.getenv('DATABASE_PASSWORD')
+db_host = os.getenv('DATABASE_HOST')
+db_port = os.getenv('DATABASE_PORT', '5432')
+db_name = os.getenv('DATABASE_NAME')
+db_connection_url = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+database_engine = create_engine(db_connection_url)
+DatabaseSession = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=database_engine))
 
-
-def get_db():
-    db = SessionLocal()
+def get_database_session():
+    db_session = DatabaseSession()
     try:
-        yield db
+        yield db_session
     finally:
-        db.close()
+        db_session.close()
 
 @lru_cache(maxsize=32)
-def get_some_data_cached(param1, param2):
-    db = next(get_db())
-    result = db.execute("SELECT * FROM some_table WHERE column1 = :param1 AND column2 = :param2",
-                        {"param1": param1, "param2": param2}).fetchall()
-    return result
+def get_records_with_cache(first_param, second_param):
+    db_session = next(get_database_session())
+    query_result = db_session.execute(
+        "SELECT * FROM some_table WHERE column1 = :first_param AND column2 = :second_param",
+        {"first_param": first_param, "second_param": second_param}
+    ).fetchall()
+    return query_result
