820317f4ab55c261e174209c5abd7c474db7ca95
EnzoFarias4 <102108717+EnzoFarias4@users.noreply.github.com>
2024-12-23 21:01:50 +0200

    error catching

--- routes.py
+++ routes.py
@@ -1,54 +1,68 @@
-from flask import Blueprint, request
-from your_idea_controller_module import create_new_idea, fetch_all_ideas, fetch_single_idea, update_existing_idea, remove_existing_idea, rate_idea, fetch_idea_rating
+from flask import Blueprint, request, jsonify, Response
+from your_idea_controller_module import (create_new_idea, fetch_all_ideas, 
+                                         fetch_single_idea, update_existing_idea, 
+                                         remove_existing_idea, rate_idea, 
+                                         fetch_idea_rating)
 
 ideas_api_blueprint = Blueprint('ideas_api', __name__)
+
+@ideas_api_blueprint.errorhandler(404)
+def resource_not_found(e):
+    return jsonify(error=str(e)), 404
+
+def validate_idea_data(idea_data):
+    return idea_data is not None and 'title' in idea_data
 
 @ideas_api_blueprint.route('/ideas', methods=['POST'])
 def add_new_idea():
     idea_data = request.get_json()
-    return create_new_idea(idea_data)
+    if not validate_idea_data(idea_data):
+        return Response("Invalid idea data", status=400)
+    response = create_new_idea(idea_data)
+    if not response:
+        return Response("Failed to create new idea", status=500)
+    return response, 201
 
 @ideas_api_blueprint.route('/ideas', methods=['GET'])
 def get_ideas():
-    return fetch_all_ideas()
+    try:
+        return fetch_all_ideas()
+    except Exception as e:
+        return jsonify(error=str(e)), 500
 
 @ideas_api_blueprint.route('/ideas/<idea_id>', methods=['GET'])
 def get_specific_idea(idea_id):
-    return fetch_single_idea(idea_id)
+    try:
+        return fetch_single_idea(idea_id)
+    except Exception as e:
+        return jsonify(error=str(e)), 404
 
 @ideas_api_blueprint.route('/ideas/<idea_id>', methods=['PUT'])
 def update_idea(idea_id):
     updated_idea_data = request.get_json()
-    return update_existing_idea(idea_id, updated_idea_data)
+    try:
+        return update_existing_idea(idea_id, updated_idea_data)
+    except Exception as e:
+        return jsonify(error=str(e)), 500
 
 @ideas_api_blueprint.route('/ideas/<idea_id>', methods=['DELETE'])
 def delete_idea(idea_id):
-    return remove_existing_idea(idea_id)
+    try:
+        return remove_existing_idea(idea_id)
+    except Exception as e:
+        return jsonify(error=str(e)), 500
 
 @ideas_api_blueprint.route('/ideas/<idea_id>/rate', methods=['POST'])
 def rate_an_idea(idea_id):
     rating_data = request.get_json()
-    return rate_idea(idea_id, rating_data)
+    try:
+        return rate_idea(idea_id, rating_data)
+    except Exception as e:
+        return jsonify(error=str(e)), 500
 
 @ideas_api_blueprint.route('/ideas/<idea_id>/rating', methods=['GET'])
 def get_idea_rating(idea_id):
-    return fetch_idea_rating(idea_id)
-```
-
-```python
-def rate_idea(idea_id, rating_data):
-    """
-    Update the provided idea with a new rating.
-    :param idea_id: The ID of the idea to be rated.
-    :param ratingody): JSON containing the rating details.
-    :return: A response indicating the success or failure of the operation.
-    """
-    pass
-
-def fetch_idea_rating(idea_id):
-    """
-    Retrieve the average rating of a specific idea.
-    :param idea_id: The ID of the idea.
-    :return: The average rating of the idea.
-    """
-    pass
+    try:
+        return fetch_idea_rating(idea_id)
+    except Exception as e:
+        return jsonify(error=str(e)), 404
